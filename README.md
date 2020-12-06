e98dbba747e54fcc9cb1ec1986000b0e4112456c
Sebastian Raaphorst <sraaphorst@gmail.com>
2020-12-06 15:16:42 -0300

    Day 1.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2020
+
+These are my solutions for the [Advent of Code 2020](https://adventofcode.com/2020/).
+
+Probably all written in Kotlin. My Kotlin is a bit rusty so earlier days may not be as clean as they could be.
---
+++ src/main/kotlin/day01/day01.kt
@@ -0,0 +1,48 @@
+package day01
+
+import java.io.File
+
+/**
+ * Find two numbers in expenseList that sum to 2020 and return their product.
+ */
+fun expenses(expenseList: List<Int>): Int? {
+    val hs = HashSet(expenseList)
+    return hs.find{ hs.contains(2020 - it) }?.let { (2020 - it) * it }
+}
+
+/**
+ * Given a collection (c0, c1, c2, ...), generate all pairs:
+ * (c0, c1), (c0, c2), ..., (c1, c2), ...
+ * If lst is infinite, since we use this ordering, we will never get to (c1, c2).
+ */
+inline fun <reified T> allPairs(lst: Collection<T>) = sequence {
+    if (lst.size > 1) {
+        val arr = lst.toTypedArray()
+        var idx0 = 0
+        var idx1 = 1
+
+        while (idx0 < arr.size - 1) {
+            if (idx1 >= arr.size) {
+                idx0 += 1
+                idx1 = idx0 + 1
+            }
+            if (idx1 < arr.size)
+                yield(Pair(arr[idx0], arr[idx1]))
+            idx1 += 1
+        }
+    }
+}
+
+fun expenses2(expenseList: List<Int>): Int? {
+    val hs = HashSet(expenseList)
+    return allPairs(expenseList).find { hs.contains(2020 - it.first - it.second) }
+        ?.let{ (2020 - it.first - it.second) * it.first * it.second}
+}
+
+fun main() {
+    println(allPairs(listOf(0, 1, 2)).toList())
+    // Answer1: 471019
+    // Answer2: 103927824
+    println(expenses(File("src/main/kotlin/day01/input.txt").readLines().map { it.toInt() }))
+    println(expenses2(File("src/main/kotlin/day01/input.txt").readLines().map { it.toInt() }))
+}
